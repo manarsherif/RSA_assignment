2305346ec5192a5afd451db2b5a123feb25578a4
manarsherif <manar.sherif93@gmail.com>
2016-12-21 14:56:42 +0200

    loans

--- src/Bank/Loans.java
+++ src/Bank/Loans.java
@@ -1,24 +1,38 @@
 package Bank;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import com.mysql.jdbc.Statement;
 
+import extras.Constants;
+
 public class Loans {
 	private Scanner scanner ;
 	private Statement stmt;
-	private Connection con;
-	Loans(Statement stmt,Connection con)
+	private Connection conn;
+	Loans()
 	{
-		// TODO Auto-generated constructor stub
-		this.scanner=new Scanner(System.in);
-		this.stmt=stmt;
-		this.con=con;
+		try {
+			Class.forName(Constants.JDBC_DRIVER);
+			conn = (Connection) DriverManager.getConnection(Constants.DB_URL, Constants.DB_USER, Constants.DB_PASS);
+			stmt = (Statement) 	conn.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
-	/*public int addNewLoan()
+	public int addNewLoan(int LoanAccNumber,float loanAmount,float interestRate,String due_date) throws SQLException
 	{
+		int LoanId=(int)(Math.random()*999999);
+		String query= "INSERT INTO Loan (LoanID, LAccNum, LAmount, LInterestRate, DueTime) VALUES	("+
+						LoanId+","+LoanAccNumber+","+loanAmount+","+interestRate+",'"+due_date+"');";
+		stmt.executeQuery(query);
+		return LoanId;
 		
-	}*/
+	}
 
 }
